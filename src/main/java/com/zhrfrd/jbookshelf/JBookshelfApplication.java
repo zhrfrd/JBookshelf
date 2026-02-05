ad356793e5fb43f0e2542c78dacb358899d11e12
Farid Zouheir <zouheirjob@hotmail.com>
2026-02-05 10:07:43 +0100

    Set up project

---
+++ src/main/java/com/zhrfrd/jbookshelf/JBookshelfApplication.java
@@ -0,0 +1,13 @@
+package com.zhrfrd.jbookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBookshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhrfrd/jbookshelf/JBookshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhrfrd.jbookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JBookshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
